b36307a4aa6ddde0e896c5f7a27fc7569e392913
Shoma SATO <s.sato@cluster.mu>
2020-05-04 03:54:43 +0900

    Add test

---
+++ Tests/Editor/HumanPoseTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using UnityEngine.TestTools.Utils;
+using System.Linq;
+
+namespace UnityHumanPoseSync.Tests.Editor
+{
+    public sealed class HumanPoseTest
+    {
+        [Test]
+        public void SerializeAndDeserializeTest()
+        {
+            var hipPosition = Random.insideUnitSphere;
+            var rotations = Enumerable.Range(0, 55).Select(_ => Random.rotation).ToArray();
+            var humanPose = new HumanPose(hipPosition, rotations);
+            var serialized = HumanPose.Serialize(humanPose);
+            var result = HumanPose.TryDeserialize(serialized, out var deserialized);
+            Assert.IsTrue(result);
+            Assert.That(deserialized.HipsPosition, Is.EqualTo(humanPose.HipsPosition).Using(Vector3EqualityComparer.Instance));
+            Assert.That(deserialized.Rotations.Length, Is.EqualTo(humanPose.Rotations.Length));
+            foreach (var (a, e) in deserialized.Rotations.Zip(humanPose.Rotations, (a, e) => (a, e)))
+            {
+                Assert.That(a, Is.EqualTo(e).Using(QuaternionEqualityComparer.Instance));
+            }
+        }
+    }
+}
